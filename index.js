c1df682e128f508e60cfde5a7722e3c7d9574c69
Hunter Wade <calebh.wade@gmail.com>
2023-04-01 21:31:05 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let greeting;
+  if (feet <= 400) {
+    greeting = 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+    greeting = 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else {
+    greeting = 'No can do.';
+  }
+  return greeting;
+}
+function ternaryCheckCity(city){
+  let response = city ===`NYC` ? `Ok, sounds good.` : `No go.`
+  return response
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
